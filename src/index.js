4772b2cf887882fcb4902767ae4a69431dcbb66b
minjung <alswjd7961@naver.com>
2022-01-28 15:02:01 +0900

    use <> tag instead of Fragment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
-import { Fragment } from 'react/cjs/react.production.min';
 
 // 이 페이지는 index.html 이 실행되고 나서 실행되는 파일 리엑트 코드들중에서 가장먼저 실행됨
 
@@ -14,12 +13,12 @@ import { Fragment } from 'react/cjs/react.production.min';
 
 // 첫번쨰 인자 html 문을 두번쨰 인자의 id 정보를보고 index.html 의 id 가 root 인 태그안에 첫번쨰 인자 값을 넣어 랜더링함
 ReactDOM.render(
-  // 반드시 한 태그로 묶여 있어야한다. 이를 위해 밖에 하나의 태그로 묶어야하는데 이를위해 Fragment 를 쓴다.
-  <Fragment>
+  // Fragment 안쓰고 아래와같이 태그를 써도 됨
+  <>
     <h1>안녕 리엑트!</h1>
     <label htmlFor="name">이름</label>
     <input id="name" type="text" onBlur="" onFocus="" onMouseDown=""/>
-  </Fragment>
+  </>
   ,
   document.getElementById('root')// dom 요소들 중에서 root 라는 id 속성을 가지고있는 요소를 가져오라 어디있냐면 index.html 의 body 태그 밑에 div 태그에 있다.
 );
