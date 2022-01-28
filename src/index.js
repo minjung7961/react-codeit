6a103f010fc6aca9ec7be32456887519cc551d48
minjung <alswjd7961@naver.com>
2022-01-28 16:37:42 +0900

    add data-status

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render( // axs 자바스크림트 코드 활용방법 표현식은 사�
   <>
     <h1> 나만의 {item} 주문하기</h1>
     <img src={imageUrl} alt="제품 사진" />
-    <button onClick={handleClick}>확인</button>
+    <button onClick={handleClick} data-status="대기중">확인</button>
   </>
   ,
   document.getElementById('root')// dom 요소들 중에서 root 라는 id 속성을 가지고있는 요소를 가져오라 어디있냐면 index.html 의 body 태그 밑에 div 태그에 있다.
