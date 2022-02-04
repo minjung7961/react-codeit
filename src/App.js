878a43a9a2be247b5339b640b21e59a7574f81e0
minjung <alswjd7961@naver.com>
2022-02-04 14:01:37 +0900

    Using React component props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dice from './Dice';
 function App(){
   return (
     <div> 
-      <Dice color="blue"/>{/* 컴포넌트에 속성지정 크롬 개발자도구 props 참조! 컴포넌트의 속성 : props, 파라미터로도 보내질수있음*/}
+      <Dice color="red" num={2} />{/* 컴포넌트에 속성지정 크롬 개발자도구 props 참조! 컴포넌트의 속성 : props, 파라미터로도 보내질수있음*/}
     </div>
   );
 }
