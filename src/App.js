2b8a8b1828999ade256ad59a25b5bd3e92e2a7aa
minjung <alswjd7961@naver.com>
2022-02-04 10:43:23 +0900

    create Dice component and show Dice component for readable code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Dice from './Dice'; // 다이스 컴포넌트 임포트
+
 function App(){
-  return <div> App 컴포넌트! </div>
+  // 리턴문에 괄호를 쓰게되면 여러줄에 걸쳐서 리턴문을 보낼수 있다.
+  return (
+    <div> 
+      <Dice />{/* 가독성 있는 컴포넌트 코드 */}
+    </div>
+  );
 }
 
 export default App;
