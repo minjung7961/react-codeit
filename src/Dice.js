19b15d7d255246fd26c99276bfaef6caa54dafa5
minjung <alswjd7961@naver.com>
2022-02-04 13:19:33 +0900

    02_15 how to write Props to console

--- src/Dice.js
+++ src/Dice.js
@@ -1,6 +1,7 @@
 import diceBlue01 from "./assets/dice-blue-1.svg";
 
-function Dice(){
+function Dice(props){ // 속성값 지정받음 props 라는 이름으로 변수를 받는건 속성값을 받는다라는 의미를 명확하게하기위한 국룰
+  console.log(props);
   return <img src={diceBlue01} alt="주사위"/>
 }
 
