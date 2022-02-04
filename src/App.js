4c4e1f4fc05aab8795cc355d2a016e3dabc0d284
minjung <alswjd7961@naver.com>
2022-02-04 13:15:09 +0900

    02_15 setting component's Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Dice from './Dice'; // 다이스 컴포넌트 임포트
+import Dice from './Dice';
 
 function App(){
-  // 리턴문에 괄호를 쓰게되면 여러줄에 걸쳐서 리턴문을 보낼수 있다.
   return (
     <div> 
-      <Dice />{/* 가독성 있는 컴포넌트 코드 */}
+      <Dice color="blue"/>{/* 컴포넌트에 속성지정 크롬 개발자도구 props 참조! 컴포넌트의 속성 : props*/}
     </div>
   );
 }
